6138d50cb10464fe80223068e480c56ba4c36c35
Scott Weitzner <scottweitzner@gmail.com>
2017-08-15 19:56:03 -0400

    removed files

--- constants.py
+++
@@ -1,8 +0,0 @@
-NEO4J_PASS = "b.btqx4xMKzzXV.Wno9p5LNGdiYFDL0"
-
-# Watson NLU Credentials
-WATSON_USER = 'b0e060ce-acb5-4d72-8606-de6a3b6f2518'
-WATSON_PASS = 'iYoVmcocpULj'
-
-# Shelve File
-SHELVE_FILE_PATH = 'watson_data_store.dat'
