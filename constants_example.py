05e5e6706346bf6090898d6b14740a8681071d2f
Scott Weitzner <scottweitzner@gmail.com>
2017-08-15 20:59:09 -0400

    adding constants example

---
+++ constants_example.py
@@ -0,0 +1,8 @@
+NEO4J_PASS = '[NEO4J PASSWORD HERE]'  # I used GrapheneDB. You may need to reconfigure if you use a different service
+
+# Watson NLU Credentials
+WATSON_USER = '[YOUR WATSON USERNAME (API KEY) HERE]'
+WATSON_PASS = '[YOUR WATSON PASSWORD HERE]'
+
+# Shelve File
+SHELVE_FILE_PATH = 'watson_data_store.dat'
